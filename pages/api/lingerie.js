f5b70146c9d93e6c4e65b4ae8eb6627444703694
Ram Murat Sharma <ram.sharma@publicissapient.com>
2020-11-23 12:42:03 +0530

    Mongodb integrated with Nextjs APIs

---
+++ pages/api/lingerie.js
@@ -0,0 +1,16 @@
+import nextConnect from 'next-connect';
+import middleware from '../middleware/database';
+
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+
+    let doc = await req.db.collection('products').findOne()
+    console.log(doc);
+    res.json(doc);
+});
+
+export default handler;
